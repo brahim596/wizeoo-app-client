60697e9ff070378378bec3c31fcfd06a46372218
Brahim <brahim.dje.59@gmail.com>
2019-03-07 22:59:59 +0100

    multiV fin

--- multi-vote.sh
+++ multi-vote.sh
@@ -2,133 +2,105 @@
 
 php vote.php kidouss 1ee31ec61d018da8a667cea06a33f052
 
+timeout=10;
 
-xterm -e openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/fr26.nordvpn.com.udp.ovpn &
+nohup openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/fr26.nordvpn.com.udp.ovpn &
 
-sleep 10
+sleep $tiemout
 
 php vote.php saitamas e32bf46fc4da6d28002221f259220969
 
-sleep 10
+sleep $timeout
 
 killall openvpn
 
-sleep 10
+sleep $timeout
 
 
 
-#***************************************************************************************
+#1***************************************************************************************
 
-xterm -e openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/fr27.nordvpn.com.udp.ovpn &
+nohup openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/fr27.nordvpn.com.udp.ovpn &
 
-sleep 10
+sleep $timeout
 
 php vote.php saitamas2 a6ea3b4add8a567ac8e96d0aacc4c4c5
 
-sleep 10
+sleep $timeout
 
 killall openvpn
 
-sleep 10 
+sleep $timeout
 
-#***************************************************************************************
+#2***************************************************************************************
 
-xterm -e openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/fr28.nordvpn.com.udp.ovpn &
+nohup openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/fr28.nordvpn.com.udp.ovpn &
 
-sleep 10
+sleep $timeout
 
 php vote.php saitamas3 d365a1b62f7c911056bf28ec3328b210
 
-sleep 10
+sleep $timeout
 
 killall openvpn
 
-sleep 10 
+sleep $timeout
 
-#***************************************************************************************
+#3***************************************************************************************
 
-xterm -e openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/fr29.nordvpn.com.udp.ovpn &
+nohup openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/be12.nordvpn.com.udp.ovpn &
 
-sleep 10
-
-php vote.php saitamas3 d365a1b62f7c911056bf28ec3328b210
-
-sleep 10
-
-killall openvpn
-
-sleep 10 
-
-#***************************************************************************************
-
-xterm -e openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/fr85.nordvpn.com.udp.ovpn &
-
-sleep 10
+sleep $timeout
 
 php vote.php saitamas4 b79d898727b012acf60ed2944ac28f8a
 
-sleep 10
+sleep $timeout
 
 killall openvpn
 
-sleep 10 
+sleep $timeout
 
-#***************************************************************************************
+#4***************************************************************************************
 
-xterm -e openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/fr30.nordvpn.com.udp.ovpn &
+nohup openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/at47.nordvpn.com.udp.ovpn &
 
-sleep 10
-
-php vote.php saitamas4 b79d898727b012acf60ed2944ac28f8a
-
-sleep 10
-
-killall openvpn
-
-sleep 10 
-
-#***************************************************************************************
-
-
-xterm -e openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/uk53.nordvpn.com.udp.ovpn &
-
-sleep 10
+sleep $timeout
 
 php vote.php spada18 5652c2ea0c14355dd20ceed83913734f
 
-sleep 10
+sleep $timeout
 
 killall openvpn
 
-sleep 10 
+sleep $timeout
 
-#***************************************************************************************
+#5***************************************************************************************
 
-xterm -e openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/uk76.nordvpn.com.udp.ovpn &
+nohup openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/au132.nordvpn.com.udp.ovpn &
 
-sleep 10
+sleep $timeout
 
 php vote.php rakikotaru dc2fb7b76f734027b3e3527f079a1e4b
 
-sleep 10
+sleep $timeout
 
 killall openvpn
 
-sleep 10 
+sleep $timeout
 
-#***************************************************************************************
+#6***************************************************************************************
 
-xterm -e openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/uk82.nordvpn.com.udp.ovpn &
+nohup openvpn /home/saitamouk/Documents/wizeoo-app-client/openvpn_udp/au193.nordvpn.com.udp.ovpn &
 
-sleep 10
+sleep $timeout
 
 php vote.php pedrolito 0f96d891f5513e891cd8f942fe97c0f2
 
-sleep 10
+sleep $timeout
 
 killall openvpn
 
-sleep 10 
+sleep $timeout
 
 #***************************************************************************************
 
